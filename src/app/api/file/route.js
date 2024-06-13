cd3a2d3835b016e927a2a8f534532af6f6e835a8
cracklesparkle <kitkatt2014@gmail.com>
2024-06-13 13:29:39 +0900

    Secure file post

--- src/app/api/file/route.js
+++ src/app/api/file/route.js
@@ -2,6 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 import { NextResponse } from 'next/server';
+import { verifyToken } from '@/app/puck/api/route';
 
 // Directory to store uploaded files
 const uploadDir = path.join(process.cwd(), 'public', 'uploads');
@@ -12,29 +13,34 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 export async function POST(request) {
-    try {
-        const formData = await request.formData();
-        const file = formData.get('file');
+    const isVerified = await verifyToken(request);
+    if (!isVerified) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    } else {
+        try {
+            const formData = await request.formData();
+            const file = formData.get('file');
 
-        if (!file) {
-            return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+            if (!file) {
+                return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+            }
+
+            // Example of handling the uploaded file
+            const fileName = file.name;
+            const filePath = path.join(uploadDir, fileName);
+
+            // Convert ArrayBuffer to Buffer
+            const buffer = Buffer.from(await file.arrayBuffer());
+
+            // Write the file to the public/uploads directory
+            await fs.promises.writeFile(filePath, buffer);
+
+            const fileUrl = `/uploads/${fileName}`;
+            return NextResponse.json({ url: fileUrl });
+        } catch (error) {
+            console.error('Error uploading file:', error);
+            return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 });
         }
-
-        // Example of handling the uploaded file
-        const fileName = file.name;
-        const filePath = path.join(uploadDir, fileName);
-
-        // Convert ArrayBuffer to Buffer
-        const buffer = Buffer.from(await file.arrayBuffer());
-
-        // Write the file to the public/uploads directory
-        await fs.promises.writeFile(filePath, buffer);
-
-        const fileUrl = `/uploads/${fileName}`;
-        return NextResponse.json({ url: fileUrl });
-    } catch (error) {
-        console.error('Error uploading file:', error);
-        return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 });
     }
 }
 
